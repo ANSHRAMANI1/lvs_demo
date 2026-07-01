076361de193f0875007cb8d3644503593c55c212
ansh <anshramani1212@gmail.com>
2026-07-01 06:47:41 +0530

    ui: chip border - lighter lime start color #C4F000 to match reference

--- lib/presentation/home/widgets/category_chip.dart
+++ lib/presentation/home/widgets/category_chip.dart
@@ -25,7 +25,7 @@ class CategoryChip extends StatelessWidget {
         decoration: isSelected
             ? BoxDecoration(
                 gradient: const LinearGradient(
-                  colors: [Color(0xFFA8E400), Color(0xFF0A6000)],
+                  colors: [Color(0xFFC4F000), Color(0xFF0A6000)],
                   begin: Alignment.bottomLeft,
                   end: Alignment.topRight,
                 ),
